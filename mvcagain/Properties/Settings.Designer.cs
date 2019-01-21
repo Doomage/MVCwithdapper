3c54eb4fae08f390322d2faeb4660d8461e8a77e
George Lymperopoulos <44376808+Doomage@users.noreply.github.com>
2019-01-21 18:25:39 +0200

    Changed STring Connection

--- mvcagain/Properties/Settings.Designer.cs
+++ mvcagain/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace mvcagain.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace mvcagain.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=DESKTOP-A9G0RJD\\SQLEXPRESS;Database=bookstore;Trusted_Connection=True;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=.\\SQLEXPRESS;Database=bookstore;Trusted_Connection=True;")]
         public string connectionString {
             get {
                 return ((string)(this["connectionString"]));
